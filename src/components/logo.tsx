144ec3d237282b688244806a9365a79b6cba992c
Taylor Hussey <Cthussey2@gmail.com>
2025-06-30 22:08:16 +0000

    Please update all relevant components to use the new SVG-based AidSync l

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,23 +1,17 @@
+import Image from 'next/image';
 import * as React from 'react';
 import { cn } from '@/lib/utils';
 
-export function Logo({ className, ...props }: React.SVGProps<SVGSVGElement>) {
+export function Logo({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <svg
-      viewBox="0 0 40 40"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className={cn(className)}
-      {...props}
-    >
-      <rect width="40" height="40" rx="8" fill="#1E5028" />
-      <path
-        d="M12 20.5L17.5 26L32.5 11"
-        stroke="#9BFFD5"
-        strokeWidth="4"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+    <div className={cn('relative', className)} {...props}>
+      <Image
+        src="/logo.svg"
+        alt="AidSync Logo"
+        fill
+        className="object-contain"
+        priority
       />
-    </svg>
+    </div>
   );
 }
